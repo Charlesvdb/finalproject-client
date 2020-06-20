c0cf0bf2255c2999da43f015825252c65f5873c7
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-06-20 15:25:42 +0200

    done

--- src/components/Challengebox.js
+++ src/components/Challengebox.js
@@ -11,27 +11,21 @@ class Challengebox extends React.Component {
         }
     }
 
-    // onDetail(challengeId){
-
-    // }
-
     render() {
         return (
-                // <Link to={`/challengedetail/${this.props.id}`}>
-                    <div className="challengebox" onClick={() => this.onDetail(this.props.id)}>    
-                            <img className="challengerpic" src="/images/charles.jpg" alt=""></img>
-                            
-                            <section className="boxchallenge">
-                                <h3>{this.props.title}</h3>
-                                <p>{this.props.description}</p>
-                            </section>
+            <div className="challengebox"> 
+                <img className="challengerpic" src="/images/charles.jpg" alt=""></img>
+                
+                <section className="boxchallenge">
+                    <h3>{this.props.title}</h3>
+                    <p>{this.props.description}</p>
+                </section>
 
-                            <div className="responsecounter">
-                                <p className="responsenumber">0</p>
-                                <p className="responsetext">Daredevils</p>
-                            </div>
-                    </div>   
-                // </Link>
+                <div className="responsecounter">
+                    <p className="responsenumber">0</p>
+                    <p className="responsetext">Daredevils</p>
+                </div>
+            </div>   
         )
     }
 }
